e6f3d7e2e229c7421526c2d996adf71e1e8b1481
mandlar <mandlar@gmail.com>
2016-10-11 14:31:09 -0400

    Implemented countdown timer

--- js/main.js
+++ js/main.js
@@ -21,56 +21,32 @@ $(document).ready(function() {
         
     
     /* ======= Countdown ========= */
-	// // set the date we're counting down to
-    // var target_date = new Date("July 1, 2017").getTime();
-     
-    // // variables for time units
-    // var days, hours, minutes, seconds;
-     
-    // // get tag element
-    // var countdown =  document.getElementById("countdown-box");
-    // var days_span = document.createElement("SPAN");
-    // days_span.className = 'days';
-    // countdown.appendChild(days_span);
-    // var hours_span = document.createElement("SPAN");
-    // hours_span.className = 'hours';
-    // countdown.appendChild(hours_span);
-    // var minutes_span = document.createElement("SPAN");
-    // minutes_span.className = 'minutes';
-    // countdown.appendChild(minutes_span);
-    // var secs_span = document.createElement("SPAN");
-    // secs_span.className = 'secs';
-    // countdown.appendChild(secs_span);
-     
-    // // update the tag with id "countdown" every 1 second
-    // setInterval(function () {
-     
-    //     // find the amount of "seconds" between now and target
-    //     var current_date = new Date().getTime();
-    //     var seconds_left = (target_date - current_date) / 1000;
-     
-    //     // do some time calculations
-    //     days = parseInt(seconds_left / 86400);
-    //     seconds_left = seconds_left % 86400;
-         
-    //     hours = parseInt(seconds_left / 3600);
-    //     seconds_left = seconds_left % 3600;
-         
-    //     minutes = parseInt(seconds_left / 60);
-    //     seconds = parseInt(seconds_left % 60);
-         
-    //     // format countdown string + set tag value.
-    //     days_span.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit script">Days</span>';
-    //     hours_span.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit script">Hrs</span>';
-    //     minutes_span.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit script">Mins</span>';
-    //     secs_span.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit script">Secs</span>';
-        
-      
-    //     //countdown.innerHTML = days + "d, " + hours + "h, "
-    //    // + minutes + "m, " + seconds + "s";  
-     
-    // }, 1000);
-     
+	// set the date we're counting down to
+    var target_date = new Date("July 1, 2017").getTime();
+    
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+
+    var days = parseInt(seconds_left / 86400);
+
+    if(days == 1) // edge cases: singularize "days" and post-marriage
+    {
+        $('#daysLeftDescription').text("day until we get married!");
+    }
+    else if(days == -1)
+    {
+        $('#daysLeftDescription').text("day married!")
+    }
+    else if (days < -1)
+    {
+        $('#daysLeftDescription').text("days married!")
+    }
+
+    if(days < 0) // make negative days into positive days (post-marriage)
+        days = days * -1;
+
+    $('#daysLeft').text(days);
 
     /* ======= Google Map ======= */
     map = new GMaps({
